4320c3db18a1ef354dca10c34f08934e2b8e5a64
Ivyne <muzendaivyne036@gmail.com>
2026-07-13 23:23:33 +0200

    saved changes

--- client.py
+++ client.py
@@ -1,35 +1,112 @@
 import socket
 import json
+import struct
+import platform
 
 HOST = '127.0.0.1'
 PORT = 6500
 
+system_info = {
+    "hostname":socket.gethostname(),
+    "platform":platform.system(),
+    "release":platform.release()
+}
+
+def send_message(module_type,message):
+
+    packet = {"module":module_type,"payload":message}
+
+    payload = json.dumps(packet).encode('utf-8')
+
+    packet_length = len(payload)
+    header = struct.pack("!I",packet_length)
+
+    s.sendall(header + payload)
+
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+
+    def recv_exact(sock,size):
+        data = b""
+
+        while len(data) < size:
+            chunk = sock.recv(size - len(data))
+
+            if not chunk:
+                return None
+
+            data += chunk
+
+        return data
+                            
+    def text_handler(packet):
+
+            payload = packet.get("payload")
+            print(f"Response: {payload}")
+
+    def system_handler(packet):
+        hostname = system_info.get("hostname")
+        platform = system_info.get("platform")
+        release = system_info.get("release")
+
+        print(f"Hostname: {hostname}","Platform: {platform}","Release: {release}")
+
+    modules = {
+        "TEXT":text_handler,
+        "SYSTEM":system_handler
+    }
 
     try:
 
         s.connect((HOST,PORT))
 
+        module_check = 1
+
+        module_to_be_used = input("module:")
+
         while True:
 
-            message = input("Type your message here: ")
+            message = input("> ")
 
-            packet = {"module":"TEXT","payload":message}
+            if message.startswith("/module"):
+                module_to_be_used = message.split(maxsplit=1)[1]
+                print(f"switched to {module_to_be_used}")
+                continue
 
-            s.sendall(
-                (json.dumps(packet) + "\n").encode('utf-8')
-                )
+            send_message(module_to_be_used,message)
 
-            data = s.recv(1024)
-            response = json.loads(data.decode('utf-8'))
-            payload = response.get("payload")
+            header = recv_exact(s,4)
 
-            if not response:
-                print("Server closed connection")
+            if header is None:
+                print("No header was found")
+                s.close()
                 break
 
-            print(f"Response: {payload}")
+            length = struct.unpack("!I",header)[0]
+
+            MAX_PACKET_LENGTH = 1024 * 1024
+
+            if length >  MAX_PACKET_LENGTH:
+                print("packet length too large ")
+                s.close()
+                break
+
+            payload = recv_exact(s,length)
+
+            data = json.loads(payload.decode('utf-8'))
+                     
+            module_name = data.get("module")
+
+            handler = modules.get(module_name)
+
+            if handler:
+                handler(data)
+            else:
+                print("unknown module")
+
+            if not data:
+                print("Server closed connection")
+                break
 
     except ConnectionRefusedError:
         print("Server not listening")        
--- server.py
+++ server.py
@@ -1,51 +1,105 @@
 import socket
 import json
+import struct
+import platform
 
 HOST = '127.0.0.1'
 PORT = 6500
 
+system_info = {
+      "hostname":socket.gethostname(),
+      "platform":platform.system(),
+            "release":platform.release()
+}
+
+def send_message(module_type,message):
+
+      packet = {"module":"TEXT","payload":message}
+
+      payload = json.dumps(packet).encode('utf-8')
+
+      packet_length = len(payload)
+      header = struct.pack("!I",packet_length)
+
+      conn.sendall(header + payload)
+
+def recv_exact(sock,size):
+      data = b""
+
+      while len(data) < size:
+            chunk = sock.recv(size - len(data))
+
+            if not chunk:
+                  return None
+
+            data += chunk
+
+      return data
+
+def text_handler(packet):
+                  
+      payload = packet.get("payload")
+      print(f"Response: {payload}")
+
+def system_handler(packet):
+      print("Client System info")
+      hostname = system_info.get("hostname")
+      platform = system_info.get("platform")
+      release = system_info.get("release")
+
+      print(f"Hostname: {hostname}",f"Platform: {platform}",f"Release: {release}")      
+
+modules = {
+      "TEXT":text_handler,
+      "SYSTEM":system_handler
+}
+
 with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s :
-    s.bind((HOST , PORT))
-    s.listen()
-    print(f"Server is listening on {HOST}:{PORT}")
+      s.bind((HOST , PORT))
+      s.listen()
+      print(f"Server is listening on {HOST}:{PORT}")
 
-    while True:
-        conn,addr = s.accept()
-        with conn:
-            print(f"Connected by {addr}")
+      while True:
+            conn,addr = s.accept()
+            with conn:
+                  print(f"Connected by {addr}")
 
-            while True:
-                    try:
+                  module_to_be_used = input("module: ")
 
-                        data = conn.recv(1024)
+                  while True:
+                              header = recv_exact(conn,4)
 
-                        if not data:
-                            break
+                              if header is None:
+                                    print("No header was found")
+                                    conn.close()
 
-                        response = json.loads(data.decode('utf-8'))
-                        module = response.get("module")
-             
-                        if module == "TEXT":
+                              length = struct.unpack("!I",header)[0]
 
-                            payload = response.get("payload")
-                            print(f"Recieved: {payload}")
-                            
-                            message = input("Type your response here: ")
-                            packet = {"module":"TEXT","payload":message}
+                              MAX_PACKET_LENGTH = 1024 * 1024
 
-                            conn.sendall((json.dumps(packet) + "\n").encode('utf-8'))
+                              if length >  MAX_PACKET_LENGTH:
+                                    print("packet length too large ")
+                                    conn.close()
+                                    break
 
+                              payload = recv_exact(conn,length)
 
-                        elif module is None:
-                            print("Packet missing module")
-                            continue
+                              data = json.loads(payload.decode('utf-8'))
 
-                    except json.JSONDecodeError as e:
-                        print(f"Invalid JSON: {e}")
-                        continue
+                              module_name = data.get("module")
 
-                    except KeyError:
-                        print("Missing 'module' key in packet")       
+                              handler = modules.get(module_name)
 
-                
+                              if handler:
+                                    handler(data)
+                              else:
+                                    print("unknown module")
             
+                              message = input("> ")
+
+                              if message.startswith("/module"):
+                                    module_to_be_used = message.split(maxsplit=1)[1]
+                                    print(f"switched to {module_to_be_used}")
+                                    continue
+
+                              send_message(module_to_be_used,message)

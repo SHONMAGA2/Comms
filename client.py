5b8295e313a074d56f92f1c0ea7c96af336e211f
Ivyne <muzendaivyne036@gmail.com>
2026-08-09 21:55:57 +0200

    testing client thread

--- client.py
+++ client.py
@@ -2,6 +2,7 @@ import socket
 from protocol import send_packet,receive_packet
 from client_config import SERVER_HOST,SERVER_PORT,DEFAULT_MODULE,SOCKET_TIMEOUT
 from handlers import build_handlers,receive_handlers
+import threading
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     try:
@@ -10,6 +11,32 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.settimeout(SOCKET_TIMEOUT)
 
         module_to_be_used = DEFAULT_MODULE
+
+        def receive_message(s):
+            while True:
+                data = receive_packet(s)
+                                
+                if data is None:
+                    print("server connection closed ")
+                    break
+                
+                module_name = data.get("module")
+                
+                handler = receive_handlers.get(module_name)
+                
+                if handler is None:
+                    print("Unknown module")
+                    continue
+                
+                handler(data)
+
+        thread = threading.Thread(
+                    target=receive_message,
+                    args=(s,)
+                )
+        thread.start()
+                
+        
         while True:
 
             message = input("> ")
@@ -33,22 +60,6 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 
             send_packet(s,packet)
             
-            data = receive_packet(s)
-
-            if data is None:
-                print("server connection closed ")
-                break
-
-            module_name = data.get("module")
-
-            handler = receive_handlers.get(module_name)
-
-            if handler is None:
-                print("Unknown module")
-                continue
-
-            handler(data)
-
 
     except ConnectionRefusedError:
         print("Server not listening")

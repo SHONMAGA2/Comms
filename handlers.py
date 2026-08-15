313713ea1b0590a989db6882048d812e8861e5d3
Ivyne <muzendaivyne036@gmail.com>
2026-08-15 16:13:05 +0200

    testing p2p com

--- handlers.py
+++ handlers.py
@@ -126,9 +126,6 @@ def dispatch_data(conn,data):
 
         system_handle(data)
 
-    elif module_name == "SYSTEM" and data_type == "CLIENT_LIST":
-        client_list_handle(conn)
-
     elif module_name == "AUTH":
         auth_handle = receive_handlers["AUTH"]
         auth_handle(conn,data)
